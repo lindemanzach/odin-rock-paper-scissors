0fbf19eeafabe3012626068b153aae95d2c1702c
lindemanzach <lindemanzachprogramming@gmail.com>
2023-02-08 15:47:07 -0500

    Create UI

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
 let gamesPlayed = 0;
+let game = true;
 
 function getComputerChoice () {
     // set computerChoice equal to a random number between 0 and 2
@@ -10,75 +11,105 @@ function getComputerChoice () {
     switch (computerChoice) {
         
         case 0:
-            computerChoice = 'rock';
+            computerChoice = 'Rock';
             break;
         
         case 1:
-            computerChoice = 'paper';
+            computerChoice = 'Paper';
             break;
         
         case 2:
-            computerChoice = 'scissors';
+            computerChoice = 'Scissors';
             break;
     }
 
     return computerChoice
 }
 
-function gameRound(playerChoice, computerChoice) {
+function playRound(event) {
 
-    // conditions for rock
-    if (playerChoice === 'rock' && computerChoice === 'rock'){
-        return `Player chooses ${playerChoice}. Computer chooses ${computerChoice}. Tie game!`;
-    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
-        computerScore++;
-        return `Player chooses ${playerChoice}. Computer chooses ${computerChoice}. Computer wins!`;
-    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-        playerScore++;
-        return `Player chooses ${playerChoice}. Computer chooses ${computerChoice}. Player wins!`;
+    // only go through this function if game is true
+
+    if (game) {
+
+        let computerChoice = getComputerChoice();
+
+        let playerChoice = event.target.id;
+        
+        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)
+
+        console.log(`Player Choice: ${playerChoice} Computer Choice ${computerChoice}`)
+
+        const GAME_STATUS = document.getElementById("game-status");
+
+        const GAME_SCORE = document.getElementById("score");
+
+
+        // conditions for rock
+        if (playerChoice === 'Rock' && computerChoice === 'Rock'){
+            GAME_STATUS.innerHTML = `Player chose: ${playerChoice} Computer chose ${computerChoice} Tie Game!`;
+        } else if (playerChoice === 'Rock' && computerChoice === 'Paper') {
+            GAME_STATUS.innerHTML = `Player chose: ${playerChoice} Computer chose ${computerChoice} Computer Wins!`;
+            computerScore++;
+        } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
+            GAME_STATUS.innerHTML = `Player chose: ${playerChoice} Computer chose ${computerChoice} Player wins!`;
+            playerScore++;
+        }
+
+        // conditions for paper
+        if (playerChoice === 'Paper' && computerChoice === 'Paper'){
+            GAME_STATUS.innerHTML = `Player chose: ${playerChoice} Computer chose ${computerChoice} Tie Game!`;
+        } else if (playerChoice === 'Paper' && computerChoice === 'Scissors') {
+            GAME_STATUS.innerHTML = `Player chose: ${playerChoice} Computer chose ${computerChoice} Computer wins!`;
+            computerScore++;
+        } else if (playerChoice === 'Paper' && computerChoice === 'Rock') {
+            GAME_STATUS.innerHTML = `Player chose: ${playerChoice} Computer chose ${computerChoice} Player wins!`;
+            playerScore++;
+        }
+
+        // conditions for scissors
+        if (playerChoice === 'Scissors' && computerChoice === 'Scissors'){
+            GAME_STATUS.innerHTML = `Player chose: ${playerChoice} Computer chose ${computerChoice} Tie Game!`;
+        } else if (playerChoice === 'Scissors' && computerChoice === 'Rock') {
+            GAME_STATUS.innerHTML = `Player chose: ${playerChoice} Computer chose ${computerChoice} Computer wins!`;
+            computerScore++;
+        } else if (playerChoice === 'Scissors' && computerChoice === 'Paper') {
+            GAME_STATUS.innerHTML = `Player chose: ${playerChoice} Computer chose ${computerChoice} Player wins!`;
+            playerScore++;
+        }
+
+        GAME_SCORE.innerHTML = `Player: ${playerScore} Computer: ${computerScore}`;
+
+        if (playerScore === 5) {
+            GAME_STATUS.innerHTML = "Player reached 5 wins first! Player wins!";
+            game = false;
+        } else if (computerScore === 5) {
+            GAME_STATUS.innerHTML = "Player reached 5 wins first! Computer wins!";
+            game = false;
+        }
     }
-
-    // conditions for paper
-    if (playerChoice === 'paper' && computerChoice === 'paper'){
-        return `Player chooses ${playerChoice}. Computer chooses ${computerChoice}. Tie game!`;
-    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
-        computerScore++;
-        return `Player chooses ${playerChoice}. Computer chooses ${computerChoice}. Computer wins!`;
-    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-        playerScore++;
-        return `Player chooses ${playerChoice}. Computer chooses ${computerChoice}. Player wins!`;
-    }
-
-    // conditions for scissors
-    if (playerChoice === 'scissors' && computerChoice === 'scissors'){
-        return `Player chooses ${playerChoice}. Computer chooses ${computerChoice}. Tie game!`;
-    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
-        computerScore++;
-        return `Player chooses ${playerChoice}. Computer chooses ${computerChoice}. Computer wins!`;
-    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-        playerScore++;
-        return `Player chooses ${playerChoice}. Computer chooses ${computerChoice}. Player wins!`;
-    }
-;}
+;} 
 
 
-while (gamesPlayed < 5) {
+function mouseEnter(event) {
+    event.target.classList.add("image-enter");
+    const audio = document.getElementById("hover");
+    audio.currentTime = 0;
+    audio.play();
 
-    let computerChoice = getComputerChoice();
-    let playerChoice = prompt("Rock, paper or scissors?").toLowerCase();
-
-    if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
-        alert(gameRound(playerChoice, computerChoice));
-        gamesPlayed++;  
-    } else {
-        alert("Please enter either rock, paper or scissors.");
-    } 
 }
 
-if (playerScore > computerScore) {
-    alert(`Player wins with a score of ${playerScore} - ${computerScore}`);
-} else if (computerScore > playerScore) {
-    alert(`Computer wins with a score of ${computerScore} - ${playerScore}`);
-} else {
-    alert(`The game is tied ${playerScore} - ${computerScore}`)
+function mouseLeave(event) {
+    event.target.classList.remove("image-enter");
 }
+
+document.getElementById("rock").addEventListener("mouseenter", mouseEnter);
+document.getElementById("rock").addEventListener("mouseleave", mouseLeave);
+document.getElementById("paper").addEventListener("mouseenter", mouseEnter);
+document.getElementById("paper").addEventListener("mouseleave", mouseLeave);
+document.getElementById("scissors").addEventListener("mouseenter", mouseEnter);
+document.getElementById("scissors").addEventListener("mouseleave", mouseLeave);
+
+document.getElementById("rock").addEventListener("click", playRound);
+document.getElementById("paper").addEventListener("click", playRound);
+document.getElementById("scissors").addEventListener("click", playRound);
